ff678b13d17b179aea0b0cdc6438190f1631c160
Sergio Jacome <sergio.jacome@gmail.com>
2020-10-08 23:59:57 -0400

    Tests for task 4

--- apps/okreads-e2e/src/specs/reading-list.spec.ts
+++ apps/okreads-e2e/src/specs/reading-list.spec.ts
@@ -1,11 +1,25 @@
-import { $, browser, ExpectedConditions } from 'protractor';
+import { $, browser, ExpectedConditions, protractor } from 'protractor';
+const until = protractor.ExpectedConditions;
+const badgeSelector = $('tmo-total-count [ng-reflect-content]');
 
 describe('When: I use the reading list feature', () => {
-  it('Then: I should see my reading list', async () => {
+  it('should add a book to my reading list', async () => {
     await browser.get('/');
     await browser.wait(
       ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
     );
+
+    const form = await $('form');
+    const input = await $('input[type="search"]');
+    await input.sendKeys('javascript');
+    await form.submit();
+
+    browser.wait(until.presenceOf($('[data-testing="book-item"]')), 500);
+
+    const addToListBtn = await $('[data-testing="book-item"] button:not([DISABLED])');
+    addToListBtn.click();
+  });
+  it('Then: I should see my reading list', async () => {
 
     const readingListToggle = await $('[data-testing="toggle-reading-list"]');
     await readingListToggle.click();
@@ -14,6 +28,17 @@ describe('When: I use the reading list feature', () => {
       ExpectedConditions.textToBePresentInElement(
         $('[data-testing="reading-list-container"]'),
         'My Reading List'
+      )
+    );
+  });
+  it('Then: should mark the book as read', async () => {
+    const markAsRead = await $('[data-testing="reading-list-mark-as-read"]');
+    await markAsRead.click();
+
+    await browser.wait(
+      ExpectedConditions.textToBePresentInElement(
+        $('[data-testing="reading-list-finished"]'),
+        'Finished on'
       )
     );
   });
